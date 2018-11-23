58e352b0952e442aa08ec4f712716a69becf5818
Sarah Currey <curreysarah28@gmail.com>
2018-11-23 17:30:13 -0500

    added other projects

--- client/src/Other.jsx
+++ client/src/Other.jsx
@@ -1,11 +1,100 @@
 import React, {Component} from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
+import './Other.css';
 
 class Other extends Component {
   render() {
-    return (
-      <div> Other </div>
+    return(
+      <React.Fragment>
+      <Nav/>
+      <div className="page">
+      <div className="amazon">
+      <header>
+  <img class="logo" src="https://duaw26jehqd4r.cloudfront.net/items/2h0q0s3f1R1v1z1h0113/Image%202018-11-23%20at%205.25.01%20PM.png?X-CloudApp-Visitor-Id=6876e04c950b8195c72dd5652dbf34ac&v=f9b4b802"/>
+  <h1>Come Back Barack: Javascript Game<i>· Front-End Developer</i></h1>
+  <span class="thumb-nugget">September 2018</span>
+  <br/>
+  <span class="thumb-nugget"><a href="https://git.generalassemb.ly/smcurrey528/come-back-barack">View on GitHub </a></span>
+  <br/>
+  <span class="thumb-nugget"> <a href="https://pages.git.generalassemb.ly/smcurrey528/come-back-barack/">Demo</a></span>
+<br/><br/><br/>
+</header>
+  <img src="https://media.giphy.com/media/lz684PNXQDXA8swONq/giphy.gif"/>
+  <br/><br/>
+
+
+<section class="row">
+
+  <div class="two-thirds column">
+    <span class="nugget">Overview</span>
+    <p><a href="http://www.airbnb.com" target="_blank">Airbnb</a> is a popular and widely known house-sharing app with over 200 million users around the world. I led and helped improve many parts of the holistic Evernote experience—from onboarding new users, to navigating and organizing content for current users, to completing checkout for new and existing customers. And now, I'm tackling the challenge of making Evernote Business the best product for teams. Brief, high-level case studies below:</p>
+  </div>
+
+
+  <div class="one-third column">
+    <span class="nugget">Hats worn</span>
+    <ul class="list">
+        <li>⛈&nbsp;&nbsp; Concepting</li>
+        <li>🗺&nbsp;&nbsp; Wireframing</li>
+        <li>💬&nbsp;&nbsp; Copywriting</li>
+        <li>♻️&nbsp;&nbsp; Prototyping</li>
+        <li>👂🏽&nbsp;&nbsp; UX research</li>
+        <li>🔸&nbsp;&nbsp; Animation</li>
+        <li>📢&nbsp;&nbsp; Visual design</li>
+        <li>🎲&nbsp;&nbsp; Hacky front-end dev</li>
+      </ul>
+  </div>
+
+</section>
+
+<hr/>
+</div>
+  <div className="amazon">
+      <header>
+  <img class="logo" src="https://i.imgur.com/EzA6UAl.png"/>
+  <h1>QualLi: Quality of Life App<i>· Front-End Developer</i></h1>
+  <span class="thumb-nugget">October 2018</span>
+  <br/>
+  <span class="thumb-nugget"><a href="https://git.generalassemb.ly/smcurrey528/finalproject">View on GitHub </a></span>
+  <br/>
+  <span class="thumb-nugget"> <a href="http://qualili.surge.sh/">Demo </a></span>
+<br/><br/><br/>
+</header>
+  <img src="https://media.giphy.com/media/17mHaT6ab91nCxo1EA/giphy.gif"/>
+  <br/><br/>
+
+
+<section class="row">
+
+  <div class="two-thirds column">
+    <span class="nugget">Overview</span>
+    <p><a href="http://www.airbnb.com" target="_blank">Airbnb</a> is a popular and widely known house-sharing app with over 200 million users around the world. I led and helped improve many parts of the holistic Evernote experience—from onboarding new users, to navigating and organizing content for current users, to completing checkout for new and existing customers. And now, I'm tackling the challenge of making Evernote Business the best product for teams. Brief, high-level case studies below:</p>
+  </div>
+
+
+  <div class="one-third column">
+    <span class="nugget">Hats worn</span>
+    <ul class="list">
+        <li>⛈&nbsp;&nbsp; Concepting</li>
+        <li>🗺&nbsp;&nbsp; Wireframing</li>
+        <li>💬&nbsp;&nbsp; Copywriting</li>
+        <li>♻️&nbsp;&nbsp; Prototyping</li>
+        <li>👂🏽&nbsp;&nbsp; UX research</li>
+        <li>🔸&nbsp;&nbsp; Animation</li>
+        <li>📢&nbsp;&nbsp; Visual design</li>
+        <li>🎲&nbsp;&nbsp; Hacky front-end dev</li>
+      </ul>
+  </div>
+
+</section>
+
+<hr/>
+</div>
+</div>
+<Footer/>
+</React.Fragment>
       )
   }
 }
-
 export default Other;
